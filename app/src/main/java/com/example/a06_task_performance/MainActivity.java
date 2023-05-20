c24b5aad9eff73325757192dbf8f05ec17906db5
Ranielle <raniellecruz65@gmail.com>
2023-05-20 15:23:45 +0800

    created layout xml and added image assets to drawables

--- app/src/main/java/com/example/a06_task_performance/MainActivity.java
+++ app/src/main/java/com/example/a06_task_performance/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.a06_task_performance;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageButton back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //back = findViewById(R.id.catButton);
     }
+
+
 }
